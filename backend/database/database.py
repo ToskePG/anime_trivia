1ccef9f971dd3af4131267a1d332121d31093306
Danilo Tosic <54001767+ToskePG@users.noreply.github.com>
2024-09-12 18:23:38 -0700

    Creates first logic for db connection

---
+++ backend/database/database.py
@@ -0,0 +1,24 @@
+# app/database/database.py
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.database.models import Base
+import logging
+
+DATABASE_URL = "mysql+mysqlconnector://danilot:Adrenalin123@127.0.0.1:3306/anime_game"
+
+# Configure logging to suppress SQLAlchemy INFO logs
+logging.getLogger("sqlalchemy.engine").setLevel(logging.WARNING)
+
+engine = create_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
