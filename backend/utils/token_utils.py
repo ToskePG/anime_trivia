3ac8764ab1d9aa6641c36ea4e43e8e869af79c38
Danilo Tosic <54001767+ToskePG@users.noreply.github.com>
2024-09-14 11:53:43 -0700

    Creates token utils.

--- backend/utils/token_utils.py
+++ backend/utils/token_utils.py
@@ -1,1 +1,17 @@
-print("This file will contain token utils.")
+from jose import jwt
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+def create_confirmation_token(email: str):
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode = {"sub": email, "exp": expire}
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
