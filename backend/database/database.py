f06073f9a4fd1b4787a1693ea8bd96d578502dab
Danilo Tosic <54001767+ToskePG@users.noreply.github.com>
2024-09-13 18:02:53 -0700

    Stores data in .env file and reads it from there.

--- backend/database/database.py
+++ backend/database/database.py
@@ -1,13 +1,27 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from backend.database.models import Base
+from dotenv import load_dotenv
 import logging
 
-DATABASE_URL = "mysql+mysqlconnector://danilot:Adrenalin123@127.0.0.1:3306/anime_game"
+# Load environment variables from the .env file
+load_dotenv()
+
+# Retrieve database credentials from environment variables
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+# Construct the DATABASE_URL from the loaded environment variables
+DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 # Configure logging to suppress SQLAlchemy INFO logs
 logging.getLogger("sqlalchemy.engine").setLevel(logging.WARNING)
 
+# Create the engine
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
