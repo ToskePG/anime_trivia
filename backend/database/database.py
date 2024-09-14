9a547b9904780d3439fd5f9d3196bd9cede0fd82
Danilo Tosic <54001767+ToskePG@users.noreply.github.com>
2024-09-13 20:20:56 -0700

    Cleans up some code.

--- backend/database/database.py
+++ backend/database/database.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from backend.database.models import Base
 from dotenv import load_dotenv
-import logging
 import os
 
 # Load environment variables from .env file
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,5 @@
-from fastapi import FastAPI, Depends, HTTPException
-from backend.database.database import init_db, get_db
-from sqlalchemy.orm import Session
+from fastapi import FastAPI
+from backend.database.database import get_db
 import logging
 from backend.database.database import Base, engine
 
@@ -14,20 +13,3 @@ app = FastAPI()
 def startup():
     # Create the tables in the database
     Base.metadata.create_all(bind=engine)
-
-@app.get("/")
-def check_health():
-    return {
-        "message": "Healthy",
-        "status": "200 OK"
-    }
-
-# Health check endpoint to test database connection
-@app.get("/db_connection")
-def check_db_connection(db: Session = Depends(get_db)):
-    try:
-        # Execute a simple query to check the connection
-        db.execute("SELECT 1")
-        return {"status": "Database connection successful"}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail="Database connection failed")
